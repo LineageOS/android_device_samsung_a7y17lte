fa241cb94442c82b576125debf1a863b0c34b1f1
filiprrs <filiprrs@gmail.com>
2018-06-15 17:32:58 +0200

    a7y17lte: Drop prebuilt device tree
    * We can compile this from source now
    
    Change-Id: I385c722c22b7989b3f9ed9cb6e36aa2fe4da7ec1

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -23,7 +23,6 @@ TARGET_SPECIFIC_HEADER_PATH += $(LOCAL_PATH)/include
 
 # Extracted with libbootimg
 BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100
-BOARD_KERNEL_PREBUILT_DT := $(LOCAL_PATH)/dtb.img
 
 # Kernel
 TARGET_KERNEL_CONFIG := lineage-a7y17lte_defconfig
